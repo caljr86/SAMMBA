3d6e7d0d6a3302172ddd9a3ec5f2929a567583a9
Cicero Alves-Lima <54912955+caljr86@users.noreply.github.com>
2026-02-10 10:01:45 +0000

    Update rsync_raw.sh

--- bash.scripts/rsync_raw.sh
+++ bash.scripts/rsync_raw.sh
@@ -8,20 +8,17 @@ PLATE="$2"
 TMP_LIST=$(mktemp)
 
 # Remote host details
-REMOTE_USER="gareth"
-REMOTE_HOST="10.36.4.90"
-REMOTE_BASE="/media/Disk2/cicero/$PLATE"
+REMOTE_USER="user"
+REMOTE_HOST="10.XX.X.YY" 
+REMOTE_BASE="/home/user/$PLATE"
 
 # Get matching files from remote "croped/" folders
 ssh $REMOTE_USER@$REMOTE_HOST "find '$REMOTE_BASE' -type f -path '*/raw/*' -name '*$PATTERN*'" > "$TMP_LIST"
 
-# Adjust the paths to be relative from $PLATE
-# For example: /media/Disk1/cicero/plate58/250706/raw/img_C13.zip
-# becomes: plate58/250706/raw/img_C13.zip
-sed -i "s|/media/Disk2/cicero/||" "$TMP_LIST"
+sed -i "s|/home/user/||" "$TMP_LIST"
 
 # Rsync matching files to current directory, preserving relative paths
-rsync -av --files-from="$TMP_LIST" --relative "$REMOTE_USER@$REMOTE_HOST:/media/Disk2/cicero/" .
+rsync -av --files-from="$TMP_LIST" --relative "$REMOTE_USER@$REMOTE_HOST:/home/user/" .
 
 # Clean up
 rm "$TMP_LIST"
