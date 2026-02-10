d4c55164557d68375e9dc4eb9383ad8f09e77811
Cicero Alves-Lima <54912955+caljr86@users.noreply.github.com>
2026-02-10 09:59:02 +0000

    Update fiji.sh

--- bash.scripts/fiji.sh
+++ bash.scripts/fiji.sh
@@ -1,2 +1,2 @@
-fiji=/home/gareth/Downloads/fiji-linux64/Fiji.app/ImageJ-linux64
+fiji=/home/user/Downloads/fiji-linux64/Fiji.app/ImageJ-linux64
 taskset -c $(shuf -i 0-$(($(nproc --all)-1)) -n $1 | paste -sd ,) $fiji
