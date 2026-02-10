d7c7b90758bca05e59fe1607adb882ef53155465
Cicero Alves-Lima <54912955+caljr86@users.noreply.github.com>
2026-02-10 10:00:46 +0000

    Update rsync_croped.sh

--- bash.scripts/rsync_croped.sh
+++ bash.scripts/rsync_croped.sh
@@ -8,20 +8,17 @@ PLATE="$2"
 TMP_LIST=$(mktemp)
 
 # Remote host details
-REMOTE_USER="gareth"
-REMOTE_HOST="10.36.4.90"
-REMOTE_BASE="/media/Disk2/cicero/$PLATE"
+REMOTE_USER="user"
+REMOTE_HOST="server IP"
+REMOTE_BASE="/home/Disk1/user/$PLATE"
 
 # Get matching files from remote "croped/" folders
 ssh $REMOTE_USER@$REMOTE_HOST "find '$REMOTE_BASE' -type f -path '*/croped/*' -name '*$PATTERN*'" > "$TMP_LIST"
 
-# Adjust the paths to be relative from $PLATE
-# For example: /media/Disk1/cicero/plate58/250706/croped/img_C13.zip
-# becomes: plate58/250706/croped/img_C13.zip
-sed -i "s|/media/Disk2/cicero/||" "$TMP_LIST"
+sed -i "s|/home/Disk1/user/||" "$TMP_LIST"
 
 # Rsync matching files to current directory, preserving relative paths
-rsync -av --files-from="$TMP_LIST" --relative "$REMOTE_USER@$REMOTE_HOST:/media/Disk2/cicero/" .
+rsync -av --files-from="$TMP_LIST" --relative "$REMOTE_USER@$REMOTE_HOST:/home/Disk1/user/" .
 
 # Clean up
 rm "$TMP_LIST"
